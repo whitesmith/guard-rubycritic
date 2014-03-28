c5edaa75b92f9511b17199a8969d77f8cb008c48
Guilherme Simoes <guilherme.rdems@gmail.com>
2014-03-28 17:42:42 +0000

    Use Rubycritic to critique

--- lib/guard/rubycritic.rb
+++ lib/guard/rubycritic.rb
@@ -1,59 +1,18 @@
 require "guard"
-require "guard/guard"
+require "guard/plugin"
+require "rubycritic"
 
 module Guard
-  class Rubycritic < Guard
 
-    # Initializes a Guard plugin.
-    # Don't do any work here, especially as Guard plugins get initialized even if they are not in an active group!
-    #
-    # @param [Array<Guard::Watcher>] watchers the Guard plugin file watchers
-    # @param [Hash] options the custom Guard plugin options
-    # @option options [Symbol] group the group this Guard plugin belongs to
-    # @option options [Boolean] any_return allow any object to be returned from a watcher
-    #
-    def initialize(watchers = [], options = {})
-      super
-    end
-
+  class Rubycritic < Plugin
     # Called once when Guard starts. Please override initialize method to init stuff.
     #
     # @raise [:task_has_failed] when start has failed
     # @return [Object] the task result
     #
     def start
+      @rubycritic = ::Rubycritic::Rubycritic.new
       UI.info "Guard::Rubycritic is critiquing"
-      run_all
-    end
-
-    # Called when `stop|quit|exit|s|q|e + enter` is pressed (when Guard quits).
-    #
-    # @raise [:task_has_failed] when stop has failed
-    # @return [Object] the task result
-    #
-    def stop
-      true
-      # DISPLAY BREAKDOWN OF WHAT CHANGED
-    end
-
-    # Called when `reload|r|z + enter` is pressed.
-    # This method should be mainly used for "reload" (really!) actions like reloading passenger/spork/bundler/...
-    #
-    # @raise [:task_has_failed] when reload has failed
-    # @return [Object] the task result
-    #
-    def reload
-      true
-    end
-
-    # Called when just `enter` is pressed
-    # This method should be principally used for long action like running all specs/tests/...
-    #
-    # @raise [:task_has_failed] when run_all has failed
-    # @return [Object] the task result
-    #
-    def run_all
-      # GET THE INITIAL SCORES
     end
 
     # Default behaviour on file(s) changes that the Guard plugin watches.
@@ -62,37 +21,8 @@ module Guard
     # @return [Object] the task result
     #
     def run_on_changes(paths)
-    end
-
-    # Called on file(s) additions that the Guard plugin watches.
-    #
-    # @param [Array<String>] paths the changes files or paths
-    # @raise [:task_has_failed] when run_on_additions has failed
-    # @return [Object] the task result
-    #
-    def run_on_additions(paths)
-      # GET THE INITIAL SCORE OF THE FILES ADDED
-    end
-
-    # Called on file(s) modifications that the Guard plugin watches.
-    #
-    # @param [Array<String>] paths the changes files or paths
-    # @raise [:task_has_failed] when run_on_modifications has failed
-    # @return [Object] the task result
-    #
-    def run_on_modifications(paths)
-      # COMPARE THE NEW SCORE WITH THE PREVIOUS SCORE OF THE FILES CHANGED
-    end
-
-    # Called on file(s) removals that the Guard plugin watches.
-    #
-    # @param [Array<String>] paths the changes files or paths
-    # @raise [:task_has_failed] when run_on_removals has failed
-    # @return [Object] the task result
-    #
-    def run_on_removals(paths)
-      # DO NOTHING?
-      true
+      UI.info "New critique at #{@rubycritic.critique(paths)}"
     end
   end
+
 end
